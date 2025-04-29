6677e1e414f31246850eb439ae259f0bcb919005
nonedalord <sokeone1@yandex.ru>
2025-04-29 04:35:14 +0300

    Changed runtime_error exception to CryptoException

--- authentication/crypto_utils/Crypto.cpp
+++ authentication/crypto_utils/Crypto.cpp
@@ -5,7 +5,7 @@ const std::string crypto::GenerateRandomKey(const size_t bytes)
 	std::vector<unsigned char> generated_array(bytes);
 	if (RAND_bytes(generated_array.data(), bytes) != 1)
 	{
-		throw std::runtime_error("Failed to generate random bytes " + std::to_string(bytes));
+		throw CryptoException("Failed to generate random bytes " + std::to_string(bytes));
 	}
 	return std::string(generated_array.begin(), generated_array.end());
 
